91bfff7b07532b27ef90836879a063c20bf4090e
Pratiksha Sonawane <98272441+Pratiksha-S-Sonawane@users.noreply.github.com>
2023-09-24 19:10:31 +0530

    Variable Creation
    Creating Variable 
    
    Print()

---
+++ var.py
@@ -0,0 +1,11 @@
+# we can directly use variable rather than declaring it
+# In python variable data type is automatically detected by using their use
+# No need of ; to end statement
+
+p=10
+
+# Here P is integer variable
+
+print("Value="+p)
+
+# print() used for display
